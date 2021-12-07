583739a45545e2f2927f8f0d2bcb4f73deb3ce43
F1L1P <78918286+F1L1Pv2@users.noreply.github.com>
2021-12-07 14:07:34 +0100

    Update 3xplus1.py

--- 3xplus1.py
+++ 3xplus1.py
@@ -1,100 +1,28 @@
-import math, time
-print("""
-              ┌─────────────────────────┐
-              │ Witaj w programie 3x+1! │
-┌─────────────┴─────────────────────────┴───────────────────┐ ┌───────────────────────────────────────────────────────────┐ ┌──────────────────────────────┐
-│                       ZASADY                              │ │                       UŻYWANIE                            │ │       3x + 1 CALCULATOR      │
-│ 1. Jeśli liczba jest parzysta to się dzieje 3x + 1.       │ │ 1. Napisz liczbę, ktora ma być obliczona.                 │ │ Autor: DuckyBlender          │
-│ 2. Jeśli liczba jest nieparzysta to się dzieli razy dwa.  │ │ 2. Jeśli wszystkie liczby po kolei sprawdzić, wpisz "ALL" │ │ Wersja: Release 2.2          │
-│ Założenie jest takie, że każda liczba dojdzie do 1.       │ │ 3. Po wpiseniu "ALL" napisz ilość liczb, lub napisz "INF" │ │ Data: 18.11.2021             │
-└───────────────────────────────────────────────────────────┘ └───────────────────────────────────────────────────────────┘ └──────────────────────────────┘""")
-firstInput = input("Podaj liczbę, lub napisz \"ALL\": ")
-times = 0
-max = 0
-
-if firstInput.isdigit():
-    temp = int(firstInput)
-    while True:
-        if temp % 2 == 0:
-            temp = temp / 2
-            times = times + 1
-            if temp >= max:
-                max = temp
-            print("Liczba: " + str(int(temp)) + " | Kroki: " + str(int(times)) + " | Max: " + str(int(max)) + " | -")
-            if (math.trunc(temp)) == 1:
-                break
-        if temp % 2 == 1:
-            temp = 3 * temp + 1
-            times = times + 1
-            if temp >= max:
-                max = temp
-            print("Liczba: " + str(int(temp)) + " | Kroki: " + str(int(times)) + " | Max: " + str(int(max)) + " | +")
-            if int(temp) == 1:
-                break
-else:
-    if firstInput.upper() == "ALL":
-        secondInput = input("Ile liczb sprawdzić? Napisz \"INF\", aby spradzić naknajwięcej: ")
-        if secondInput.isdigit():
-            maxNumber = int(secondInput)
-            number = int(1)
-            temp = number
-            while maxNumber >= number:
-                if temp % 2 == 0:
-                    temp = temp / 2
-                    times = times + 1
-                    if temp >= max:
-                        max = temp
-                    if int(temp) == 1:
-                        print("Liczba: " + str(int(number)) + " | Kroki: " + str(int(times)) + " | Max: " + str(int(max)))
-                        max = 0
-                        times = 0
-                        number = number + 1
-                        temp = number
-                if temp % 2 == 1:
-                    temp = 3 * temp + 1
-                    times = times + 1
-                    if temp >= max:
-                        max = temp
-                    if int(temp) == 1:
-                        print("Liczba: " + str(int(number)) + " | Kroki: " + str(int(times)) + " | Max: " + str(int(max)))
-                        max = 0
-                        times = 0
-                        number = number + 1
-                        temp = number
-        else:
-            if secondInput.upper() == "INF":
-                number = int(1)
-                temp = number
-                print("Eksplozja za 3 sekundy...\n")
-                time.sleep(3)
-                while True:
-                    if temp % 2 == 0:
-                        temp = temp / 2
-                        times = times + 1
-                        if temp >= max:
-                            max = temp
-                        if (int(temp)) == 1:
-                            print("Liczba: " + str(int(number)) + " | Kroki: " + str(int(times)) + " | Max: " + str(int(max)))
-                            max = 0
-                            times = 0
-                            number = number + 1
-                            temp = number
-                    if temp % 2 == 1:
-                        temp = 3 * temp + 1
-                        times = times + 1
-                        if temp >= max:
-                            max = temp
-                        if (int(temp)) == 1:
-                            print("Liczba: " + str(int(number)) + " | Kroki: " + str(int(times)) + " | Max: " + str(int(max)))
-                            max = 0
-                            times = 0
-                            number = number + 1
-                            temp = number
-                    exit
-            else:
-                print("ERROR 2: Niewłaściwy input! Przujmuję wartości: Liczba, \"INF\".")
-                exit
+# -*- coding: UTF-8 -*-
+def threexplusone(number, fn):
+  n = 1
+  max = 1
+  while number != 1:
+    if number % 2 == 0:
+      number = number / 2
     else:
-        print("ERROR 1: Niewłaściwy input! Przujmuję wartości: Liczba, \"ALL\".")
-        exit
-print("Pomyślnie zakończono zadanie.")
+      number = number * 3 + 1
+    if number > max:
+      max = number
+    n += 1
+  print("Number: " + str(fn) + " | Steps: " + str(n) + " | Max: " + str(int(max)))
+numberType = input("              ┌────────────────────────┐\n              │    Welcome to  3x+1!   │\n┌─────────────┴────────────────────────┴────┐ ┌───────────────────────────────────────────────────────────┐ ┌──────────────────────────────┐\n│                       RULES               │ │                       HOW TO USE                          │ │       3x + 1 CALCULATOR      │\n│ 1. If a number is even do: 3x + 1         │ │ 1. Type a number that will be checked.                    │ │ Authors: DuckyBlender, F1L1P │\n│ 2. If a number is odd do: /2              │ │ 2. If you want to check a portion of numbers, type \"ALL\"  │ │ Version: Release 3.0         │\n│ Every number will eventually land on 1.   │ │ 3. After typing \"ALL\", type \"INF\" to check all numbers    │ │ Date: 07.12.2021             │\n└───────────────────────────────────────────┘ └───────────────────────────────────────────────────────────┘ └──────────────────────────────┘\n\nInput a number or type \"ALL\": ")
+if numberType.upper() == "ALL":
+  count = input('ile liczb / inf: ')
+  temp = 1
+  if count.upper() == "INF":
+    while 1:
+      threexplusone(float(temp), int(temp))
+      temp += 1
+  else:
+    count = int(count)
+    while temp <= count:
+      threexplusone(float(temp), temp)
+      temp += 1
+else:
+  threexplusone(float(numberType), numberType)
